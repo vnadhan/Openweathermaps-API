1120d37d9e971655d0ac17218fa505ccca2bcf23
unknown <vshgemini@gmail.com>
2018-03-04 15:18:10 +0100

    cleaned code

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Dependencies
 var express = require('express');
-var router = express.Router();
 var http = require('http'); // HTTP 
 var axios = require('axios'); // for making http requests
 
 // Current app
 var app = express();
+var router = express.Router();
 
 // Templating Engine
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 
+// Initializations
 var BASE_API = "http://api.openweathermap.org/data/2.5/";
 var API_KEY = "b4aafa28db3c260e9b1256c547534e87";
 var API_KEY_URL_PARAM = "&APPID=";
@@ -24,17 +25,21 @@ app.get('/', (req, res) => {
     res.render('weather', { forecastData : "", text: "" })
 });
 
+// get forecast weather of a city
 app.get('/city', function(req, res) {
     var city = req.query.cityname;
     
     if(req.query.cityname != "" && req.query.cityname != undefined) {
         console.log("cityname is " + city);
+        // build API
         var api = BASE_API + "forecast?q=" + city + API_RESPONSE_MODE + API_KEY_URL_PARAM + API_KEY
         console.log("API - " + api);
+        
         // Call API to fetch forecast data
         axios.get(api)
             .then(response => {
                 console.log("Success - " + response.data.city.name + ", " + response.data.city.country);
+                // build the templating data
                 res.render('weather', 
                            { forecastData : {
                                 list: response.data.list, 
@@ -46,17 +51,19 @@ app.get('/city', function(req, res) {
                 );
             })
             .catch(error => {
+                // Error in finding weather details
                 console.error("ERROR : " + error);
                 res.render('weather', { text : "Could not fetch weather details for " + city });
             }
         );
     } else {
+        // cityname cannot be empty
         console.error("Cityname is empty!");
         res.render('weather', { text : "Please enter a valid city!" });
     }
 });
 
-// Server
+// Server defintion
 http.createServer(app).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
